1f8d84c749fd58b991dcc0fe92d4f253dde3aaa5
Bryan Sory <bryansory@icloud.com>
2016-11-17 12:17:50 -0600

    models complete

--- Models/Models.cs
+++ Models/Models.cs
@@ -11,53 +11,56 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-public class Card : HasId
+public class User : HasId
 {
     [Required]
     public int Id { get; set; }
     [Required]
-    public string Title { get; set; }
+    public string Name { get; set; }
     [Required]
-    [StringLength(250, MinimumLength = 10)]
-    public string Text { get; set; }
-
-    public int CardListId {get;set;}
+    [StringLength(50, MinimumLength = 10)]
+    public string tagLine { get; set; }
+    public string Avatar {get;set;}
+    public string Email {get;set;}
+    public string Password {get; set;}
+    public List <Room> Rooms {get; set;}
+    public List<Message> Messages {get; set; }
 }
-
-public class CardList : HasId {
+public class Room : HasId {
     [Required]
     public int Id { get; set; }
     [Required]
     public string Summary { get; set; }
     [Required]
-    public List<Card> Cards { get; set; }
+    public List<User> Users { get; set; }
+    public List<Message> Messages { get; set; }
 
-    public int BoardId {get;set;}
+    public int UserId {get;set;}
 }
-
-public class Board : HasId {
+public class Message : HasId {
     [Required]
     public int Id { get; set; }
     [Required]
-    public string Title { get; set; }
+    public string Text { get; set; }
     [Required]
-    public List<CardList> Lists { get; set; }
+    public int RoomId {get; set; }
+    public int UserId {get; set; }
+    public DateTime createdAt {get; set; }
 }
 
 // declare the DbSet<T>'s of our DB context, thus creating the tables
 public partial class DB : IdentityDbContext<IdentityUser> {
-    public DbSet<Card> Cards { get; set; }
-    public DbSet<CardList> CardLists { get; set; }
-    public DbSet<Board> Boards { get; set; }
+    public DbSet<User> Cards { get; set; }
+    public DbSet<Message> Messages { get; set; }
+    public DbSet<Room> Rooms { get; set; }
 }
 
 // create a Repo<T> services
 public partial class Handler {
     public void RegisterRepos(IServiceCollection services){
-        Repo<Card>.Register(services, "Cards");
-        Repo<CardList>.Register(services, "CardLists",
-            d => d.Include(l => l.Cards));
-        Repo<Board>.Register(services, "Boards",
-            d => d.Include(b => b.Lists).ThenInclude(l => l.Cards));
+        Repo<User>.Register(services, "Users");
+        Repo<Message>.Register(services, "Messages");
+        Repo<Room>.Register(services, "Rooms",
+            d => d.Include(b => b.Users).ThenInclude(l => l.Messages));
     }
 }
