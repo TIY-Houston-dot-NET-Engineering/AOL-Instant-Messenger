f13d29b19ca18f62749b32c00fe9ca3cd2ed0835
Bryan Sory <bryansory@icloud.com>
2016-11-18 16:19:52 -0600

    test

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,6 @@ import fetch from "isomorphic-fetch"
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 import { Router, Route, Link, browserHistory, hashHistory } from 'react-router'
-import * as BLUE from '@blueprintjs/core'
-
-console.log(BLUE);
 
 // Utility methods
 // --------------
@@ -14,7 +11,9 @@ const log = (...a) => console.log(...a)
 
 const get = (url) =>
     fetch(url, {credentials: 'same-origin'})
-    .then(r => r.json())
+    .then(r => {
+        return r.json()
+    })
     .catch(e => log(e))
 
 const post = (url, data) => 
@@ -28,63 +27,117 @@ const post = (url, data) =>
     .then(r => r.json())
 // ----------------
 
-const Nav = () => 
-    <nav className="pt-navbar pt-dark pt-fixed-top">
-        <div className="pt-navbar-group pt-align-left">
-            <div className="pt-navbar-heading">AOL Msngr</div>
-            <input className="pt-input" placeholder="Search rooms..." type="text" />
+const Tweet = (tweet) => 
+    <a className="tweet" href={`#/status/${tweet.id}`}>
+        <p>{tweet.text}</p>
+        <span>{tweet.user}</span>
+    </a>
+
+class Home extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            items: []
+        }
+    }
+    componentDidMount(){
+        get('/api/tweet').then(tweets => {
+            tweets = tweets.reverse()
+            this.setState({items: tweets})
+        }).catch(e => log(e))
+    }
+    render(){
+        return <div className="grid grid-3-600">
+            {this.state.items.map(Tweet)}
         </div>
-        <div className="pt-navbar-group pt-align-right">
-            <button className="pt-button pt-minimal pt-icon-home">Home</button>
-            <button className="pt-button pt-minimal pt-icon-document">Files</button>
-            <span className="pt-navbar-divider"></span>
-            <button className="pt-button pt-minimal pt-icon-user"></button>
-            <button className="pt-button pt-minimal pt-icon-notifications"></button>
-            <button className="pt-button pt-minimal pt-icon-cog"></button>
+    }
+}
+
+class TweetPage extends Component {
+    constructor(props){
+        super(props)
+        this.state = { id: props.params.id } 
+    }
+    componentDidMount(){
+        get('/api/tweet/'+this.state.id).then(x => {
+            this.setState({ item: x })
+        })
+    }
+    render() {
+        const item = this.state.item
+        if(!item)
+            return <div/>
+
+        return <div className="tweet">
+            <h5>{item.user}</h5>
+            <hr/>
+            <p>{item.text}</p>
         </div>
-    </nav>
+    }
+}
 
-const Breadcrumbs = () =>
-    <ul className="pt-breadcrumbs">
-        {["Home", "About", "Story"].map(x => 
-            <li><BLUE.Breadcrumb text={x} /></li>
-        )}
-    </ul>
-
-const LogIn = ({title="", message="", inputName = "", inputPass = ""}) => 
-    <div className="pt-card pt-elevation-1 pt-interactive">
-        <h5>{title}</h5>
-        <p>{message}</p>
-        <input type="text" class="pt-input" placeholder="Username" />
-        <input type="password" class="pt-input" placeholder="Password" />
-        <button class="pt-button pt-large pt-intent-primary">Login</button>
-    </div>
-
-
-const Home = () => 
+const Layout = ({children}) => 
     <div>
-        <Nav />
-        <Breadcrumbs />
-        <hr />
-        <div className="grid">
-            {[
-                {title: "Welcome", message: "Please Login, Sweetcheeks", inputName: "Ted", inputPass: "dick"}
-                
-            ].map(x => [<LogIn {...x} />, " "] )}
+        <div className="menu">
+            <h6>Matter</h6>
+            <a href="#/compose" className="compose-button"/>
         </div>
-        
-        
+        {children}
     </div>
 
+const Error = () => <div>Page Not Found</div>
+
+class ComposePage extends Component {
+    constructor(props){
+        super(props)
+        this.state = {}
+    }
+    submit(e){
+        e.preventDefault()
+        post('/api/tweet', {
+            text: this.refs.text.value,
+            user: this.refs.user.value
+        }).then(x => {
+            if(!x.errors) window.location.hash = `#/status/${x.id}`
+
+            this.setState({ errors: x.errors })
+        }).catch(e => alert(e))
+    }
+    render(){
+        var err 
+        if(this.state.errors){
+            err = <ul className="compose-errors">
+                    {this.state.errors.map(x => <li>{x}</li>)}
+                </ul>
+        }
+
+        return <form className="compose-screen" onSubmit={e => this.submit(e)}>
+
+            {this.state.errors ? <p>There were errors with your tweet:</p> : null}
+            {err}
+
+            <div>
+                <input ref="user" type="text" placeholder="Username" required />
+                <textarea ref="text" placeholder="Your message" required></textarea>
+            </div>
+            <div>
+                <button type="submit">Live Tweet or Die Tryin'</button>
+            </div>
+        </form>
+    }
+}
 const reactApp = () => 
     render(
-    <Router history={hashHistory}>
-        <Route path="/" component={Home}/>
-    </Router>,
+        <Layout>
+            <Router history={hashHistory}>
+                <Route path="/" component={Home}/>
+                <Route path="/status/:id" component={TweetPage}/>
+                <Route path="/compose" component={ComposePage}/>
+                <Route path="*" component={Error}/>
+            </Router>
+        </Layout>,
     document.querySelector('.app'))
-
 reactApp()
-
 // Flow types supported (for pseudo type-checking at runtime)
 // function sum(a: number, b: number): number {
 //     return a+b;
